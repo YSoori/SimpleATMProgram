06c26045710e19b15a53c5c2d7d82eed3ca03cba
YANG JIHYEON <83569982+YSoori@users.noreply.github.com>
2021-09-14 10:49:36 +0900

    Add files via upload

---
+++ ATM_Test.java
@@ -0,0 +1,126 @@
+package C0901_Practice_Acc;
+
+import java.util.*;
+
+
+public class ATM_Test {
+	
+	static void showAcc(Account[] arr, int cnt) {
+		boolean check=false;
+		for(int i=0;i<cnt;i++) {
+			System.out.println(arr[i].name + " / " + arr[i].acc);
+			check=true;
+		}
+		if(check==false) {
+			System.out.println("등록된 계좌가 없습니다.");
+		}
+	}
+	
+	static Account findAcc(Account[] arr, int cnt, String acc) {
+		for(int i=0;i<cnt;i++) {
+			if(arr[i].acc.equals(acc)) {
+				return arr[i];
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		String acc, name, pw, sendA;
+		int nowMoney, money, cnt=0;
+		Account[] account = new Account[100];
+		Account curr = null, sendAcc = null;
+		
+		Scanner sc = new Scanner(System.in);
+	
+		int but1 = 0;
+		
+		System.out.println("ATM 프로그램을 실행합니다.");
+		while(but1!=4) {
+			int but2 = 0;
+			System.out.println("1.계좌 등록 2.모든 계좌 조회 3.로그인 4.종료");
+			but1 = sc.nextInt();
+			
+			switch(but1) {
+			case 1: 
+				System.out.println("예금주 : "); name = sc.next();
+				System.out.println("계좌번호 : "); acc = sc.next();
+				if(findAcc(account, cnt, acc)==null) {
+					System.out.println("비밀번호 : "); pw = sc.next();
+					System.out.println("입금할 금액 : "); money = sc.nextInt();
+					account[cnt] = new Account(acc, name, pw, money);
+					System.out.println(account[cnt].name+"님의 계좌가 생성되었습니다.");
+					cnt++;
+				}else {
+					System.out.println("이미 등록된 계좌번호입니다.");
+				}
+				break;
+			case 2:
+				showAcc(account, cnt);
+				break;
+			case 3:
+				if(account[0]==null) {
+					System.out.println("등록된 계좌가 없습니다.");
+				}else {
+					System.out.println("계좌번호 : "); acc=sc.next();
+					curr = findAcc(account, cnt, acc);
+					if(curr!=null) {
+						System.out.println(curr.name+"님 반갑습니다.");
+						System.out.println("다음 화면으로 이동합니다.");
+						
+						while(but2!=5) {
+							System.out.println("1.조회 2.입금 3.출금 4.송금 5.로그아웃");
+							but2=sc.nextInt();
+							
+							switch(but2) {
+							case 1:
+								System.out.println("비밀번호 : "); pw = sc.next();
+								curr.showMoney(pw);
+								break;
+							case 2:
+								System.out.println("입금액 : "); money = sc.nextInt();
+								curr.credit(money);
+								System.out.println(money+"원 입급되었습니다.");
+								break;
+							case 3:
+								System.out.println("비밀번호 : "); pw = sc.next();
+								System.out.println("출금액 : "); money = sc.nextInt();
+								
+								if(curr.debit(pw, money)!=0) {
+									System.out.println(money+"원 출금되었습니다.");
+								}
+								
+								break;
+							case 4: 
+								System.out.println("송금할 계좌 : "); sendA = sc.next();
+								System.out.println("송금액 : "); money = sc.nextInt();
+								System.out.println("비밀번호 : "); pw = sc.next();
+								sendAcc = findAcc(account, cnt, sendA);
+								if(sendAcc!=null) {
+									curr.send(sendAcc, pw, money);
+								}
+								break;
+							case 5: 
+								System.out.println("처음 화면으로 돌아갑니다.");
+								break;
+							default:
+								System.out.println("올바른 번호를 입력해주세요.");
+								break;
+							}
+						}
+					}else {
+						System.out.println("등록되지 않은 계좌번호입니다.");
+					}
+				}
+				break;
+			case 4:
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			default:
+				System.out.println("올바른 번호를 입력해주세요.");
+				break;
+			
+			}
+		}
+	}
+}
---
+++ Account.java
@@ -0,0 +1,62 @@
+package C0901_Practice_Acc;
+
+public class Account {
+	String acc, name, pw; // 계좌번호, 이름,비밀번호
+	int nowMoney, money=0; //잔고, 입출금에 사용할 돈
+	int minusLine; //마이너스 통장
+	
+	
+	//생성자 계좌번호, 이름, 비밀번호, 금액을 입력받고 초기화를 함.
+	public Account(String acc, String name, String pw, int nowMoney) {
+		this.acc = acc;
+		this.name = name;
+		this.pw = pw;
+		this.nowMoney = nowMoney;
+	}
+	
+	//생성자 다형성 금액을 입력받지 않으면 금액을 0으로 리턴한다.
+	public Account(String acc, String name, String pw) {
+		this(acc, name, pw, 0);
+	}
+	
+	//입금 메소드
+	public int credit(int money) {
+		return this.nowMoney+=money; 
+	}
+	
+	//출금 메소드, 비밀번호와 보낼금액을 매개변수로 받고 확인 후 출금 실행 or 실패처리.
+	public int debit(String pw, int money) {
+		if(this.pw.equals(pw)) {
+			if(this.nowMoney-money>=0) {
+				return this.nowMoney-=money;
+			}else {
+				System.out.println("잔액이 부족합니다.");
+			}
+		}else {
+			System.out.println("잘못된 비밀번호 입니다.");
+		}
+		
+		return 0;
+	}
+	
+	//송금 메소드, 상대방계좌, 비밀번호, 보낼금액을 매개변수로 받아옴
+	//상대방계좌와 비밀번호 확인 후 메소드 실행 or 실패처리
+	public boolean send(Account sendAcc, String pw, int money) {
+		int result = this.debit(pw, money);
+		if(result!=0) {
+			sendAcc.credit(money);
+			return true;
+		}
+		return false;
+	}
+	
+	//잔고를 보여주는 메소드
+	public void showMoney(String pw) {
+		if(this.pw.equals(pw)) {
+			System.out.println(this.name+"님의 잔고는"+this.nowMoney+"원 입니다." );
+		}else {
+			System.out.println("잘못된 비밀번호 입니다.");
+		}
+	}
+	
+}
